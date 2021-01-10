47d233b7df5c9884d5e8eba6d84e71668306c3b8
yourimlee <67430267+yourimlee@users.noreply.github.com>
2021-01-10 19:30:16 +0900

    2021_01_10

---
+++ Programmers/k번째수.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    for i in commands:
+        ans=array[i[0]-1:i[1]]
+        ans.sort()
+        answer.append(ans[i[2]-1])
+        
+    return answer

36d6b5d3fe04a19109e3f5ce90e5e8574ad5aefa
yourimlee <yourim100415@naver.com>
2022-01-28 17:46:44 +0900

    3주차 bfs/dfs

--- Baekjoon/BFS/DFS/연결요소의개수.py
+++ Baekjoon/BFS/DFS/연결요소의개수.py
@@ -1,6 +1,25 @@
 import sys
 sys.setrecursionlimit(10000)
+from collections import deque
 
 n, m = map(int, input().split())
-
+link = [[] for _ in range(n+1)]
 visited = [0] * (n + 1)
+for _ in range(m):
+    x, y = map(int, input().split())
+    link[x].append(y)
+    link[y].append(x)
+cnt = 0
+
+def dfs(v):
+    visited[v] = 1
+    for i in link[v]:
+        if visited[i] == 0:
+            dfs(i)
+            
+for j in range(1, n+1):
+    if visited[j] == 0:
+        dfs(j)
+        cnt += 1
+        
+print(cnt)

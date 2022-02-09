5b1f33ef5b56a0c8809ab1b830d5a8abb9ec4b5b
yourimlee <yourim100415@naver.com>
2022-02-09 16:02:06 +0900

    5주차 DP

--- Baekjoon/DP/계단오르기.py
+++ Baekjoon/DP/계단오르기.py
@@ -4,12 +4,17 @@ score = []
 for _ in range(n):
     score.append(int(input()))
 
-dp = []
-dp.append(score[0])
-dp.append(max(score[0]+score[1], score[1]))
-dp.append(max(score[0]+score[2], score[1]+score[2]))
+if n == 1:
+    print(score[0])
+elif n == 2:
+    print(score[0]+score[1])
+else:
+    dp = []
+    dp.append(score[0])
+    dp.append(max(score[0]+score[1], score[1]))
+    dp.append(max(score[0]+score[2], score[1]+score[2]))
 
-for i in range(3, n):
-    dp.append(max(dp[i-2] + score[i], dp[i-3] + score[i] + score[i-1]))
+    for i in range(3, n):
+        dp.append(max(dp[i-2] + score[i], dp[i-3] + score[i] + score[i-1]))
 
-print(dp.pop())
+    print(dp.pop())

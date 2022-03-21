8e75c43b90910378fbd3a49e20ac4a8d220b0057
yourimlee <yourim100415@gmail.com>
2022-03-21 23:46:15 +0900

    Class 3

---
+++ Baekjoon/Class3/팩토리얼0의개수.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+ans = 0
+for i in range(1, n+1):
+    if i % 5 == 0:
+        ans += 1
+    if i % 25 == 0:
+        ans += 1
+    if i % 125 == 0:
+        ans += 1
+print(ans)

090364c037c1044ce81f9bb65a0f245b0fb486e4
yourimlee <yourim100415@gmail.com>
2022-03-22 00:00:36 +0900

    Class 3

--- Baekjoon/Class3/듣보잡.py
+++ Baekjoon/Class3/듣보잡.py
@@ -8,10 +8,9 @@ for _ in range(n):
     li_1.add(sys.stdin.readline())
 for _ in range(m):
     li_2.add(sys.stdin.readline())
-    
-res = sorted(list(li_1 & li_2))
 
+res = sorted(list(li_1 & li_2))
 print(len(res))
 
 for i in res:
-    print(i)
+    print(i, end = '')

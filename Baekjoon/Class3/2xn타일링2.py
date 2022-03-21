c59c99de7b72562e3203279f84ec3d19b3d52f80
yourimlee <yourim100415@gmail.com>
2022-03-21 23:14:29 +0900

    Class 3

---
+++ Baekjoon/Class3/2xn타일링2.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+dp = [0] * (n+1)
+if n == 1:
+    dp[1] = 1
+elif n == 2:
+    dp[1], dp[2] = 1, 3
+else:
+    dp[1], dp[2] = 1, 3
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + 2 * dp[i-2]
+
+print(dp[n] % 10007)

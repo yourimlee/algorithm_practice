7e910591b2590dc1c801543f06e9933838b80d0e
yourimlee <yourim100415@naver.com>
2022-04-06 23:19:21 +0900

    Class 3

---
+++ Baekjoon/Class3/dfs와bfs.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+def dfs(v, visited=[]):
+    visited.append(v)
+    print(v, end=' ')
+    for i in range(1, n+1):
+        if graph[v][i] == 1 and i not in visited:
+            dfs(i, visited)
+
+def bfs(v):
+    visited = [v]
+    queue = deque()
+    queue.append(v)
+    while queue:
+        v = queue.popleft()
+        print(v, end = ' ')
+        for i in range(1, n+1):
+            if graph[v][i] == 1 and i not in visited:
+                visited.append(i)
+                queue.append(i)
+
+n, m, v = map(int, input().split())
+graph = [[0] * (n+1) for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = graph[b][a] = 1
+
+dfs(v)
+print()
+bfs(v)

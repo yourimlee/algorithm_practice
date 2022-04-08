47c81299a06c6e2f136dc41c893fcd20883696ce
yourimlee <yourim100415@naver.com>
2022-04-08 17:12:20 +0900

    Level 2

--- Programmers/2022/기능개발.py
+++ Programmers/2022/기능개발.py
@@ -13,6 +13,3 @@ def solution(progresses, speeds):
     answer.append(len(days) - cnt)
 
     return answer
-
-print(solution([93, 30, 55], [1, 30, 5]))
-print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
---
+++ Programmers/2022/문자열압축.py
@@ -0,0 +1,26 @@
+def solution(s):
+    result = []
+    if len(s) == 1:
+        return 1
+    for i in range(1, len(s)//2 + 1):
+        c = ''
+        cnt = 1
+        tmp = s[:i]
+        for j in range(i, len(s), i):
+            if tmp == s[j:i+j]:
+                cnt += 1
+            else:
+                if cnt != 1:
+                    c += str(cnt) + tmp
+                else:
+                    c += tmp
+                tmp = s[j:i+j]
+                cnt = 1
+        if cnt != 1:
+            c += str(cnt) + tmp
+        else:
+            c += tmp
+
+        result.append(len(c))
+        
+    return min(result)

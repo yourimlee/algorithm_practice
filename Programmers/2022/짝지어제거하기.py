da328b9bf922e626d276b81a99d8b10f77bec609
yourimlee <yourim100415@naver.com>
2022-04-08 18:29:04 +0900

    Level 2

---
+++ Programmers/2022/짝지어제거하기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    stack = []
+    for c in s:
+        if len(stack) == 0: stack.append(c)
+        elif stack[-1] == c:
+            stack.pop()
+        else:
+            stack.append(c)
+    if len(stack) == 0:
+        return 1
+    else: return 0

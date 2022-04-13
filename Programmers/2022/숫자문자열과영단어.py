997cdadef011604adbbfe7546cb1aa53ef27ada6
yourimlee <yourim100415@naver.com>
2022-04-13 20:50:41 +0900

    Class 2

---
+++ Programmers/2022/숫자문자열과영단어.py
@@ -0,0 +1,8 @@
+def solution(s):
+    strings = [('zero', '0'), ('one', '1'), ('two', '2'), ('three', '3'), ('four', '4'), ('five', '5'), ('six', '6'), ('seven', '7'), ('eight', '8'), ('nine', '9')]
+    while s.isdigit() == False:
+        for i in range(len(strings)):
+            if strings[i][0] in s:
+                s = s.replace(strings[i][0], strings[i][1])
+        return int(s)
+    return int(s)

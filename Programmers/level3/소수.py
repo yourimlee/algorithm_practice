a56e671fcdaa33412a520355916df9bf0e5cec62
yourimlee <yourim100415@naver.com>
2022-04-28 17:30:57 +0900

    소수 만들기 복습

---
+++ Programmers/level3/소수.py
@@ -0,0 +1,16 @@
+## 에라토스테네스의 체
+
+def is_prime_num(n):
+    arr = [True] * (n + 1) # 특정 수가 지워졌는지 아닌지 확인하기 위한 배열
+    arr[0] = False
+    arr[1] = False
+
+    for i in range(2, n + 1):
+        if arr[i] == True: # 특정 수가 지워지지 않았다면 (소수여서)
+            j = 2
+
+            while (i * j) <= n:
+                arr[i*j] = False # i의 배수의 값을 False로 지워준다.
+                j += 1
+
+    return arr
